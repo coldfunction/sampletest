345bf179deda3f7ede2f60e8c24990457433b4ba
Yu-Shiang Lin <coldfunction@Yu-ShiangtekiMacBook-Pro.local>
2016-10-17 12:55:18 +0800

    first commit

---
+++ e1.c
@@ -0,0 +1,41 @@
+
+ 
+ 
+/**
+ * @param
+ * @param
+ * @param
+ */
+void quicksort(int *data, int left, int right)
+{
+	if(left >= right) return;
+
+	int pivot, i, j;
+	pivot = data[left];
+
+	i = left + 1;
+	j = right;
+
+	while(1)
+	{
+		while(i <= right)
+		{
+			if(data[i] > pivot)
+				break;
+			i++;
+
+		}tes
+		while(j > left)
+		{
+			if(data[j] < pivot)
+				break;
+			j--;
+		}
+		if(i>j) break;
+		SWAP(&data[i], &data[j]);
+	}
+	SWAP(&data[left], &data[j]);
+
+	quicksort(data, left, j-1);
+	quicksort(data, j+1, right);
+}
